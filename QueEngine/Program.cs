f5b9809ae1bc3f12a3b85bc00c6e4269c2c9e900
John Rosenbaum <jrosenbaum@smg.com>
2012-06-02 15:46:21 -0500

    Que using Quartz

--- QueEngine/Program.cs
+++ QueEngine/Program.cs
@@ -1,17 +1,79 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading;
 using Common.Logging;
 using Quartz;
 using Quartz.Impl;
+using Commuticate.Repository;
+using Commuticate;
 
 namespace Commuticate.Que
 {
 
     public class EmailSender : IJob
     {
+		private static readonly ILog Log = LogManager.GetLogger(typeof(EmailSender));
+
         public void Execute(IJobExecutionContext context)
         {
-            throw new NotImplementedException();
+			Log.Info(string.Format("Start! - {0}", System.DateTime.Now.ToString("r")));
+
+            var queDay = DateTime.Now.ToString("ddd").ToUpperInvariant();
+            var queLastRun = context.PreviousFireTimeUtc;
+            var queTimeEnd = context.NextFireTimeUtc;
+
+            using (var ent = new Commuticate.Repository.Entities())
+            {
+                //var comm =
+                //    ent.Commuters.FirstOrDefault(c => c.Email.Equals("jrosenbaum@smg.com", StringComparison.OrdinalIgnoreCase));
+
+                //var rGroup = new RouteGroup() {Commuter = comm, Description = "First Route Group"};
+                //ent.AddToRouteGroups(rGroup);
+
+                //var route = new Route() {Commuter = comm, Description = "First Route"};
+                //ent.AddToRoutes(route);
+
+                //var rGroupR = new RouteGroupRoute() {RouteGroup = rGroup, RouteId = route.Id};
+                //ent.AddToRouteGroupRoutes(rGroupR);
+                //ent.SaveChanges();
+
+                //var rGroup = ent.RouteGroups.FirstOrDefault(g => g.Description == "First Route Group");
+                //var que = new Repository.Que() {Description = "John", RouteGroup = rGroup};
+                //var sch = new Repository.QueSchedule() {AtTime = DateTime.Now.TimeOfDay, NotifyEmail = true, OnDays = queDay, Que = que};
+
+                //ent.AddToQues(que);
+                //ent.AddToQueSchedules(sch);
+                //ent.SaveChanges();
+
+                
+
+                var queSchedule =
+                    (from q in ent.QueSchedules
+                     where q.OnDays.Contains(queDay)
+                     //&& (q.AtTime >= queLastRun.TimeOfDay && q.AtTime < queTimeEnd.TimeOfDay)
+                     select q);
+
+
+                using (var smtp = new SmtpClient("mail.tecknonerd.com"))
+                {
+                    smtp.Credentials = new NetworkCredential("hackthemidwest@tecknonerd.com", "fredthefish");
+
+                    foreach (var schedule in queSchedule)
+                    {
+                        smtp.Send(new MailMessage("hackthemidwest@tecknonerd.com",
+                                                  schedule.Que.RouteGroup.Commuter.Email,
+                                                  "Commuticate Que",
+                                                  "It worked: " + DateTime.Now));
+                    }
+                }
+            }
+
+			Log.Info(string.Format("End! - {0}", System.DateTime.Now.ToString("r")));
+
+            //Console.CursorLeft = 0;
+            Console.WriteLine(String.Format("Scheduled: {0} - Ran: {1} ", context.ScheduledFireTimeUtc, context.FireTimeUtc));
         }
     }
 
@@ -44,6 +106,7 @@ namespace Commuticate.Que
             ITrigger trigger = TriggerBuilder.Create()
                 .WithIdentity("trigger1", "group1")
                 .StartAt(runTime)
+                .WithSimpleSchedule(x => x.WithIntervalInMinutes(5).RepeatForever())
                 .Build();
 
             // Tell quartz to schedule the job using our trigger
@@ -60,18 +123,17 @@ namespace Commuticate.Que
             log.Info("------- Waiting 65 seconds... -------------");
 
             // wait 65 seconds to show jobs
-            Console.WriteLine("Sleeping");
-            Thread.Sleep(TimeSpan.FromSeconds(65));
-
-
+            Console.WriteLine("Running");
+            Console.WriteLine("Press any key to shutdown");
+            Console.WriteLine("");
+            Console.Read();
 
             // shut down the scheduler
             log.Info("------- Shutting Down ---------------------");
             sched.Shutdown(true);
             log.Info("------- Shutdown Complete -----------------");
 
-            Console.WriteLine("Done");
-            Console.Read();
+
 
 
 
@@ -113,7 +175,7 @@ namespace Commuticate.Que
             //    var queSchedule =
             //        (from q in ent.QueSchedules
             //         where q.OnDays.Contains(queDay)
-            //            //&& (q.AtTime >= queLastRun.TimeOfDay && q.AtTime < queTimeEnd.TimeOfDay)
+            //         //&& (q.AtTime >= queLastRun.TimeOfDay && q.AtTime < queTimeEnd.TimeOfDay)
             //         select q);
 
 
